ebf5c03f42d082c6817e523d4ec9cb364bb1e321
Dykann <dykann3@hotmail.fr>
2021-04-20 16:13:28 +0200

    add extra ex

--- src/color.js
+++ src/color.js
@@ -1,20 +1,28 @@
-const colors = document.querySelectorAll(".w3-col.l4.m6.w3-center.colorbox");
-const arr = [...colors];
+const colorsNode = document.querySelectorAll(
+  ".w3-col.l4.m6.w3-center.colorbox"
+);
 
-const colorsObj = arr.map((color) => {
+const colorsObj = [...colorsNode].map((color) => {
   return {
     name: color.querySelector(".colornamespan").innerText,
     hex: color.querySelector(".colorhexspan").innerText,
   };
 });
 
+// const colorsObj = [...colorsNode].map((color, index) => {
+//   return {
+//     name: document.querySelectorAll(".colornamespan")[index].innerText,
+//     hex: document.querySelectorAll(".colorhexspan")[index].innerText,
+//   };
+// });
+
 const div = document.querySelector("#main");
 
-div.outerHTML = colorsObj
-  .map((obj, i) => {
+div.innerHTML = colorsObj
+  .map((color) => {
     return `<div>
-  <p style="background-color:${obj.hex}">${obj.name}</p>
-  <span style="background-color:${obj.hex}"></span>
+  <p>${color.name}</p>
+  <span style="background-color:${color.hex};display:inline-block;width:300px;height:100px"></span>
   </div>`;
   })
   .join("");
